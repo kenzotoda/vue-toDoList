2e9d3df083a8e296240285273f436ad56212f589
Kenzo Ribeiro Toda <kenzoribeirotoda@gmail.com>
2025-10-30 00:28:44 -0300

    Criando os components e simulando api e banco de dados com json-serve

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,31 @@
 <script setup>
+import ToDoEmpty from './components/ToDoEmpty.vue';
+import ToDoFormAdd from './components/ToDoFormAdd.vue';
+import ToDoItems from './components/ToDoItems.vue';
+import ToDoSpinner from './components/ToDoSpinner.vue';
 
-import HelloWorld from './components/HelloWorld.vue'
 
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  
+    <!-- Content -->
+    <div class="px-3 py-10 md:px-10">
+        <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+            <ToDoSpinner></ToDoSpinner>
+
+            <ToDoFormAdd></ToDoFormAdd>
+
+            <ToDoItems></ToDoItems>
+
+            <ToDoEmpty></ToDoEmpty>  
+        </div>
     </div>
-    
-  </header>
+    <!--/ Content -->
+
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ src/components/ToDoItems.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import ToDoItem from '@/components/ToDoItem.vue';
+</script>
+
+<template>
+    <div class="space-y-2">
+        <ToDoItem></ToDoItem>      
+        <ToDoItem></ToDoItem>
+    </div>
+</template>
